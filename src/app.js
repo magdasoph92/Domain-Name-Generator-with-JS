953a95ed21434937e897a1f30bd8bf81e3e8bc2c
magdasoph92 <137792219+magdasoph92@users.noreply.github.com>
2023-08-16 19:23:28 +0000

    Domain Generator with JS

--- src/app.js
+++ src/app.js
@@ -2,10 +2,21 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pron = ["the", "our", "my"];
+  let adj = ["awesome", "amazing", "perfect", "breathtaking"];
+  let noun = ["racoon", "beatles", "slide", "kettle"];
+  let dot = [".com", ".org", ".net"];
+
+  for (let pronIndex = 0; pronIndex < pron.length; pronIndex++) {
+    for (let adjIndex = 0; adjIndex < adj.length; adjIndex++) {
+      for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
+        for (let dotIndex = 0; dotIndex < dot.length; dotIndex++) {
+          console.log(
+            pron[pronIndex] + adj[adjIndex] + noun[nounIndex] + dot[dotIndex]
+          );
+        }
+      }
+    }
+  }
 };
