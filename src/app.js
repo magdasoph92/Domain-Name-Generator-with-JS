9fa934f91f7940fc664c07b34e4f527601181224
magdasoph92 <137792219+magdasoph92@users.noreply.github.com>
2023-08-24 16:34:39 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,18 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
+onclick = function() {
   let pron = ["the", "our", "my"];
   let adj = ["awesome", "amazing", "perfect", "breathtaking"];
   let noun = ["racoon", "beatles", "slide", "kettle"];
   let dot = [".com", ".org", ".net"];
+  var separator = "<br><br>";
 
   for (let pronIndex = 0; pronIndex < pron.length; pronIndex++) {
     for (let adjIndex = 0; adjIndex < adj.length; adjIndex++) {
       for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
         for (let dotIndex = 0; dotIndex < dot.length; dotIndex++) {
-          console.log(
-            pron[pronIndex] + adj[adjIndex] + noun[nounIndex] + dot[dotIndex]
+          document.write(
+            pron[pronIndex] +
+              adj[adjIndex] +
+              noun[nounIndex] +
+              dot[dotIndex] +
+              separator
           );
         }
       }
